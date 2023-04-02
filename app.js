6ba0205128ffc5d2c6afe6f553456e2fffe95cd9
CodingGeeneeus <126993260+CodingGeeneeus@users.noreply.github.com>
2023-04-02 12:36:47 -0600

    created inputNum function to display number

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var inputScreen = document.getElementById('screenContent')
+
+function inputNum(num) {
+    if (inputScreen.textContent.length >= 15) return;
+    return inputScreen.textContent += num
+     
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
