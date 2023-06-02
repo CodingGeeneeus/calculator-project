062561bde007bcc1e498ba48631fa77be240de9a
windowsCurtis <curtisod.123@gmail.com>
2023-06-02 10:47:36 -0600

    Small bug fixes with calculator screen

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
+const inputScreen = document.getElementById("screenContent");
+const numButtons = document.querySelectorAll("[data-numbuttons]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const clearButton = document.querySelector("[data-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const equalsButton = document.querySelector("[data-equals]");
 
-var inputScreen = document.getElementById('screenContent');
-const numButtons = document.querySelectorAll('[data-numbuttons]');
-const operationButtons = document.querySelectorAll('[data-operation]')
-const clearButton = document.querySelector('[data-clear]');
-const deleteButton = document.querySelector('[data-delete]');
-const equalsButton = document.querySelector('[data-equals]')
+let previousOperandText = document.querySelector("[data-previous-operand]");
+let currentOperandText = document.querySelector("[data-current-operand]");
 
-let previousOperandText = document.querySelector('[data-previous-operand]')
-let currentOperandText = document.querySelector('[data-current-operand]')
+let displayValue = document.getElementById("screenContent");
 
-let displayValue = document.getElementById('screenContent')
+const calculator = new Calculator(previousOperandText, currentOperandText);
 
-const calculator = new Calculator(previousOperandText, currentOperandText)
+numButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    if (calculator.currentOperandText.includes(".") && button == ".") return;
+    calculator.appendNumber(button.textContent);
+    calculator.updateDisplay();
+  })
+);
 
-numButtons.forEach(button => button.addEventListener('click', () => {
-    if(calculator.currentOperandText.includes('.') && button == '.') return;
-    calculator.appendNumber(button.textContent)
-    calculator.updateDisplay()
-}))
+operationButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.textContent);
+    calculator.updateDisplay();
+  })
+);
 
-operationButtons.forEach(button => button.addEventListener('click', () => {
-    calculator.chooseOperation(button.textContent)
-    calculator.updateDisplay()
-}))
-
-deleteButton.addEventListener('click' , () => {
-    calculator.delete()
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
 });
 
-equalsButton.addEventListener('click', () => {
-    calculator.compute()
-    calculator.updateDisplay()
-})
+equalsButton.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
 
-clearButton.addEventListener('click', () => {
-    calculator.clear()
-    calculator.updateDisplay()
-})
+clearButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
